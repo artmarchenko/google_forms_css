564ed6c1d8b35cf711ce2ba0fbbeb75585b7d73c
Artem Marchenko <art.marchenko@gmail.com>
2019-04-21 23:50:20 +0300

    add new form in index2.html; change ajax.js func request

--- ajax.js
+++ ajax.js
@@ -11,18 +11,40 @@ function sendAjaxForm(result_form, ajax_form, url) {
     $.ajax({
         url:     url, //url страницы (action_ajax_form.php)
         type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        dataType: "xml", //формат данных
+        //data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        data:{
 
-        success: function(response) { //Данные отправлены успешно
-        	//result = response;
-          //$('#showresults').replaceWith($('#showresults').html(response));
-           alert(response.status);
-        	$('#result_form').html('Данные отправлены ))');
-    	},
-    	error: function(response) { // Данные не отправлены
-        alert(response.status);
-            $('#result_form').html('Данные yt отправлены ))');
-    	}
+          "entry.1308696487" : $('#firstInput').val(),
+          "entry.474829116" : $('#secondInput').val()
+
+        },
+        statusCode: {
+                0: function() {
+                    //Success message
+                    alert("0!")
+                },
+                404: function() {
+                    //Success Message
+                  //  alert("404!")
+                  $('#result_form').html('Данные отправлены ))');
+                },
+                200: function() {
+                    //Success Message
+                    alert("200!")
+                }
+            },
+
+      //   success: function(response) { //Данные отправлены успешно
+      //   	//result = response;
+      //     //$('#showresults').replaceWith($('#showresults').html(response));
+      //      alert(response.status);
+      //   	$('#result_form').html('Данные отправлены ))');
+    	// }
+      //,
+    	// error: function(response) { // Данные не отправлены
+      //   alert(response.status);
+      //       $('#result_form').html('Данные yt отправлены ))');
+    	// }
  	});
 }
